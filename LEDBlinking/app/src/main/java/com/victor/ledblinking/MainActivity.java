6fc7e662a30d2cf87c011f08f8178cc137498ca8
Javier Gamarra <nhpatt@gmail.com>
2017-11-24 04:04:31 +0100

    clean and sort

--- LEDBlinking/app/src/main/java/com/victor/ledblinking/MainActivity.java
+++ LEDBlinking/app/src/main/java/com/victor/ledblinking/MainActivity.java
@@ -3,66 +3,31 @@ package com.victor.ledblinking;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 import java.io.IOException;
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * mLedGpio.setValue(true);
- * }</pre>
- *
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- */
 public class MainActivity extends Activity {
 
-	private Gpio gpio;
+	private Gpio led;
 	private Handler handler = new Handler();
-	private static final String LED = "BCM6";
-
-	private Runnable blinkingRunnable = new Runnable() {
-		@Override
-		public void run() {
-			if (gpio == null) {
-				return;
-			}
-
-			try {
-				gpio.setValue(!gpio.getValue());
-
-				handler.postDelayed(blinkingRunnable, 1000);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	};
+	private static final String LED_ADDRESS = "BCM6";
+	public static final String APP = "APP";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		PeripheralManagerService service = new PeripheralManagerService();
-
 		try {
-			gpio = service.openGpio(LED);
-			gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+			PeripheralManagerService service = new PeripheralManagerService();
+			led = service.openGpio(LED_ADDRESS);
+			led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
 
 			handler.post(blinkingRunnable);
 		} catch (IOException e) {
-			e.printStackTrace();
+			Log.e(APP, e.getMessage(), e);
 		}
 	}
 
@@ -72,16 +37,33 @@ public class MainActivity extends Activity {
 
 		handler.removeCallbacks(blinkingRunnable);
 
-		if (gpio == null) {
+		if (led == null) {
 			return;
 		}
 
 		try {
-			gpio.close();
+			led.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			Log.e(APP, e.getMessage(), e);
 		} finally {
-			gpio = null;
+			led = null;
 		}
 	}
+
+	private Runnable blinkingRunnable = new Runnable() {
+		@Override
+		public void run() {
+			if (led == null) {
+				return;
+			}
+
+			try {
+				led.setValue(!led.getValue());
+
+				handler.postDelayed(blinkingRunnable, 1000);
+			} catch (IOException e) {
+				Log.e(APP, e.getMessage(), e);
+			}
+		}
+	};
 }
