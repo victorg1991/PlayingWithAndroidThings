c0e2b667d7c66922289fa7784e3f6ad48ee170ad
Javier Gamarra <nhpatt@gmail.com>
2017-11-24 07:06:43 +0100

    clean java file

--- Catapult/app/src/main/java/com/victor/catapult/MainActivity.java
+++ Catapult/app/src/main/java/com/victor/catapult/MainActivity.java
@@ -3,40 +3,19 @@ package com.victor.catapult;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import com.google.android.things.contrib.driver.pwmservo.Servo;
-import com.google.android.things.pio.PeripheralManagerService;
-import com.google.android.things.pio.Pwm;
 import java.io.IOException;
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * PeripheralManagerService service = new PeripheralManagerService();
- * mLedGpio = service.openGpio("BCM6");
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
- * mLedGpio.setValue(true);
- * }</pre>
- *
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- */
 public class MainActivity extends Activity {
 
 	private Servo servo;
+	private static final String APP = "APP";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		PeripheralManagerService service = new PeripheralManagerService();
 
 		try {
 			servo = new Servo("PWM1");
@@ -54,9 +33,8 @@ public class MainActivity extends Activity {
 			SystemClock.sleep(1000);
 
 			servo.setAngle(90);
-
 		} catch (IOException e) {
-			e.printStackTrace();
+			Log.e(APP, e.getMessage(), e);
 		}
 	}
 
@@ -67,9 +45,8 @@ public class MainActivity extends Activity {
 		try {
 			servo.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally {
+			Log.e(APP, e.getMessage(), e);
+		} finally {
 			servo = null;
 		}
 	}
