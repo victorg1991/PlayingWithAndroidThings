d3484875a1983019ea2f730162ce70b3784fe5df
Pablo Molina <contact@pablomolina.me>
2018-03-03 15:41:41 +0100

    Add button management to MainActivity

--- LEDBlinking/app/src/main/java/com/victor/ledblinking/MainActivity.java
+++ LEDBlinking/app/src/main/java/com/victor/ledblinking/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.KeyEvent;
+
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.button.ButtonInputDriver;
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 import java.io.IOException;
@@ -14,6 +18,7 @@ public class MainActivity extends Activity {
 	private Handler handler = new Handler();
 	private static final String LED_ADDRESS = "BCM6";
 	public static final String APP = "APP";
+	private ButtonInputDriver driver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,20 @@ public class MainActivity extends Activity {
 			PeripheralManagerService service = new PeripheralManagerService();
 			led = service.openGpio(LED_ADDRESS);
 			led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-
-			handler.post(blinkingRunnable);
 		} catch (IOException e) {
 			Log.e(APP, e.getMessage(), e);
+		}
+
+		try {
+			this.driver = new ButtonInputDriver(
+                "BCM21",
+                Button.LogicState.PRESSED_WHEN_LOW,
+                KeyEvent.KEYCODE_SPACE
+            );
+
+			driver.register();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -48,6 +63,36 @@ public class MainActivity extends Activity {
 		} finally {
 			led = null;
 		}
+
+		if (driver != null) {
+			driver.unregister();
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		super.onKeyUp(keyCode, event);
+
+		try {
+			this.led.setValue(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		super.onKeyUp(keyCode, event);
+
+		try {
+			this.led.setValue(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return true;
 	}
 
 	private Runnable blinkingRunnable = new Runnable() {
